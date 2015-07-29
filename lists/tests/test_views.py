7fdf9e8ab51a603da85a12f6d4c202e7692ef48f
Hao Chen <hao.chen@avalara.com>
2015-07-29 00:43:30 -0700

    Using ExistingListItemForm in list view

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,7 +5,10 @@ from django.http import HttpRequest
 from django.utils.html import escape
 from lists.views import home_page, new_list
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    ItemForm, EMPTY_ITEM_ERROR,
+    DUPLICATE_ITEM_ERROR, ExistingListItemForm,
+    )
 from unittest import skip
 
 # Create your tests here.
@@ -102,7 +105,7 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_errors_on_page(self):
         response = self.post_invalid_input()
@@ -111,16 +114,15 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/%d/' % (list_.id,))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
-        respone = self.client.post('/lists/%d/' % (list1.id,),
+        response = self.client.post('/lists/%d/' % (list1.id,),
                                    data={'text': 'textey'})
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
