6353d313f00eea35947125aab5b0efce1e5346e4
Hao Chen <hao.chen@avalara.com>
2015-07-30 23:38:52 -0700

    the first cut of FT to fix error with typing and refactoring

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -37,3 +37,23 @@ class ItemValidationTest(FunctionalTest):
         self.check_for_row_in_list_table('1: Buy wellies')
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You've already got this in your list")
+
+    def test_error_messages_are_cleared_on_input(self):
+        #Start a new list in a way that causes a validation error
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        #start typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+
+        #verify that error message disappeares
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
+
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
+        
+        
