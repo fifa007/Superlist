629b0ba8bfc07eaf952f17d9b83c94faf27e7d40
Hao Chen <hao.chen@avalara.com>
2015-08-16 20:04:59 -0700

    small tweak for FT

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -8,7 +8,7 @@ class LoginTest(FunctionalTest):
         # User goes to the awsome list
         # and notices the 'Sign in' link for the first time
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('login').click()
+        self.browser.find_element_by_id('id_login').click()
 
         # A Persona sign in box appears
         self.switch_to_new_window('Mozilla Persona')
@@ -24,7 +24,7 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
 
         # User has successfully logged in
-        self.wait_for_element_with_id('logout')
+        self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn('edith@mockmyid.com', navbar.text)
 
